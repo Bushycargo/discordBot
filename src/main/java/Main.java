ca5fef51e7c5198f895dd5c3fdae6b8b2960a71a
Bushycargo <josephaines03@gmail.com>
2021-01-13 22:40:39 +0000

    Slight cleanup of the Main.java file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,11 @@ import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
+        //SET UP ---------
         String BOT_TOKEN = "NDEzNDAwOTcyNTgxMjczNjEx.WoR_qA.TLgsaz3WnzB32Fp2oNBDW1ncxLY";
+        String PREFIX = "yo ";
+
         JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
-        api.addEventListener(new Listener("."));
+        api.addEventListener(new Listener(PREFIX));
     }
 }

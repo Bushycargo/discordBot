644e3c3f471ee28bb845f45b204886bb92146408
Bushycargo <josephaines03@gmail.com>
2021-01-14 15:21:20 +0000

    Now has a settings.json file meaning that the token isn't hard coded

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import javax.security.auth.login.LoginException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws Exception {
-        //SET UP ---------
-        String BOT_TOKEN = "NDEzNDAwOTcyNTgxMjczNjEx.WoR_qA.TLgsaz3WnzB32Fp2oNBDW1ncxLY";
-        String PREFIX = "yo ";
-        Integer VERBOSITY = 2; //0 = No output, 1 = Minimal, 2 = Commands ran output, 3 = Every message read output
+        JSONObject settings;
+        try {
+            settings = (JSONObject) new JSONParser().parse(new FileReader("settings.json"));
+        }
+        catch (IOException e){
+            settings = (JSONObject) new JSONParser().parse(new FileReader("build/libs/settings.json"));
+        }
+        String BOT_TOKEN = settings.get("BOT_TOKEN").toString();
+        String PREFIX = settings.get("PREFIX").toString();
+        Integer VERBOSITY = 2;
         JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
         api.addEventListener(new Listener(PREFIX, VERBOSITY));
     }
